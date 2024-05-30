f06780a35e3ca6be65180deeb4d27506e8d3f3b4
MatthewZhu13 <68396952+MatthewZhu13@users.noreply.github.com>
2024-05-29 18:28:16 -0700

    Update TwoSum.py

--- Arrays/TwoSum.py
+++ Arrays/TwoSum.py
@@ -1,12 +1,28 @@
 class Solution(object):
     def twoSum(self, nums, target):
+
+        #Variable that is: target - value you are currently
+        #Determines what value you need to find in the array
         remainder = 0
+
+        #Stores previously iterated over values and their indexes
+        """dict = {
+            7 : 0
+            2 : 1
+            4 : 2
+        }
+        """
         dict = {}
 
         for index, value in enumerate(nums):
+            #Determines what value you need to find in the array
             remainder = target - value
+
+            #Looks back in dict to see if you remainder is a value you have iterated over already in the array
             if remainder in dict:
+                #Output statement
                 return(index, dict[remainder])
             else:
+                #If remainder is not in dict, store the current value and index in dict and move on
                 dict[value] = index
         
