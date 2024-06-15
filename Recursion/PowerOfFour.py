cbe4746e032b99143d327a72b31ce38276cb18aa
MatthewZhu13 <68396952+MatthewZhu13@users.noreply.github.com>
2024-06-14 18:45:07 -0700

    Update PowerOfFour.py

--- Recursion/PowerOfFour.py
+++ Recursion/PowerOfFour.py
@@ -1,11 +1,11 @@
 def powerOfFour(self, n, x = 1):
   #If constant x is ever > 4 and has not = 4 yet, then it is not a power of 4 
   if x > n:
-    return True
+    return False
 
-  #If negative numbers and 0 are not a power of 4
-  if n <= 0:
-      return False
+  #Base case to stop the recursive cycle
+  if n == x:
+      return True
 
   #Recursive statement increasing constant x until it etiher equals or is > 4
   return self.powerOfFour(n, x * 4)
