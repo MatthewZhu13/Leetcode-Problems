54a826bf876a1a5e270ce350c24d2a37c09f04da
MatthewZhu13 <68396952+MatthewZhu13@users.noreply.github.com>
2024-07-29 11:04:49 -0700

    Update SqrtX.py

--- Binary Search/SqrtX.py
+++ Binary Search/SqrtX.py
@@ -1,31 +1,25 @@
 class Solution(object):
-    def mySqrt(self, x):
+        def mySqrt(self, x):
+        if x <= 1:
+            return x
+
         #Create left and right variables
         l, r = 0, x
 
         #Binary search
         while l <= r:
             #Calculate middle variable
-            m = (l + r) // 2
-
+            m = (l + r) / 2
             #mOver is for if the answer is a float
             mOver = m + 1
 
-            if m * m == x:
-                return m
-            
-            #Check if answer is between m and mOver (determines if the answer is a float)
-            elif m * m < x and mOver * mOver > x:
-                #if answer is a float, return rounded down version of the float (m)
-                return m
-            
+            #Check if sqrt x is m or check if answer is between m and mOver (determines if the answer is a float)
+            if m * m == x or (m * m < x and mOver * mOver > x): break
+
             #If m * m is > target, move the right variable down since m is too big
-            elif m * m > x:
-                r = m - 1
+            elif m * m > x: r = m - 1
 
             #If m * m is < target, move the left variable up since m is too small
-            else:
-                l = m + 1
+            else: l = m + 1
             
-        return -1
-        
+        return m
